7a2464f6dfb048b4583b071a86472ae6d152779e
Ugo Barbato <jugobarbato@gmail.com>
2021-07-23 20:38:19 +0200

    Edited the file for the preprocessing of data

--- Preprocessing.R
+++ Preprocessing.R
@@ -4,10 +4,24 @@ myds = read.csv('RegressionData_SDA_AH_group3.csv')
 # Scale the dataset by subtracting the column means and dividing by the column 
 # standard deviation
 myds = scale(myds)
+myds = as.data.frame(myds)
 
 # Separate the dataset in X (containing predictors) and Y (containing response)
 X = myds[, 1:10]
 Y = myds[, 11:18]
+
+# Rename column names of X and Y datasets
+library(dplyr)
+X <- rename(X, Temperature=X_Temperature, Humidity=X_Humidity, Altitude=X_Altitude,
+            ClimaticConditions=X_ClimaticConditions, RestTimeFromLastMatch=X_RestTimeFromLastMatch,
+            AvgPlayerValue=X_AvgPlayerValue, MatchRelevance=X_MatchRelevance, 
+            AvgGoalConcededLastMatches=X_AvgGoalConcededLastMatches, SupportersImpact=X_SupportersImpact,
+            OpposingSupportersImpact=X_OpposingSupportersImpact)
+
+Y <- rename(Y, Dehydration=Y_Dehydration, Hyperthermia=Y_Hyperthermia, 
+            AvgSpeed=Y_AvgSpeed, AvgTravelledDistance=Y_AvgTravelledDistance, 
+            PressingCapability=Y_PressingCapability, PhysicalEndurance=Y_PhysicalEndurance,
+            MentalConcentration=Y_MentalConcentration, EmotionalMotivation=Y_EmotionalMotivation)
 
 # Split the two datasets in train and test
 splitpoint = 30
@@ -15,3 +29,4 @@ X_train = X[1:splitpoint,]
 X_test = X[(splitpoint+1):40,]
 Y_train = Y[1:splitpoint,]
 Y_test = Y[(splitpoint+1):40,]
+
