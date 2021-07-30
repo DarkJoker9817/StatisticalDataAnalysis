5508260e12130ca37c57eb8c8a0c4149d1547e05
Ugo Barbato <jugobarbato@gmail.com>
2021-07-30 14:57:59 +0200

    fix

--- Regression/LeaveOneOutCrossValidation.R
+++ Regression/LeaveOneOutCrossValidation.R
@@ -4,7 +4,7 @@ library(boot)
 cv.error.LOOCV=rep(0,25)
 for (i in 1:25){
   glm.fit=glm(models[[i]], data=myds)
-  cv.error.LOOCV[i]=cv.glm(myds, glm.fit)$delta[1]
+  cv.error.LOOCV[i]=cv.glm(myds, glm.fit, K=40)$delta[1]
 }
 print(cv.error.LOOCV)
 
